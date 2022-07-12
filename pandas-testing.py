ee749167a71f29372348ea89498f12c50b4b6e47
Andres Tomas Abastante <atabastante@gmail.com>
2022-07-12 16:07:41 -0300

    asd

--- pandas-testing.py
+++ pandas-testing.py
@@ -1,1 +1,15 @@
-import panda as pd
+import pandas as pd
+
+# Read data from .csv file
+data = pd.read_csv('/Users/andresabastante/codigo/pandas-testing/IMDB-Movie-Data.csv')
+
+# Read data with specified explicit index.
+# We will use this later in our analysis
+data_indexed = pd.read_csv('/Users/andresabastante/codigo/pandas-testing/IMDB-Movie-Data.csv', index_col="Title")
+
+print(data_indexed)
+test=data_indexed.loc[['Suicide Squad']][['Genre','Actors','Director','Rating','Revenue (Millions)']]
+
+print(test)
+test=data.iloc[10:15][['Title','Rating','Revenue (Millions)']]
+print(test)
